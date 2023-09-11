e7ffbdef94986c7ac9017644664033270190a8b0
Felipe Amorim <felipeamorim.go@gmail.com>
2023-09-11 19:27:31 -0300

    Menu hb adicionado

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,5 @@ const menuBtn = document.querySelector(".header__meuhb");
 const navList = document.querySelector(".header__nav")
 menuBtn.addEventListener("click", () => {
   navList.classList.toggle("ativo")
+  menuBtn.classList.toggle("menu-opacity")
 })
