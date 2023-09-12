0be43d89465b61a83db89d225e08fefbbed8bd93
Felipe Amorim <felipeamorim.go@gmail.com>
2023-09-12 10:21:01 -0300

    css otimizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const menuBtn = document.querySelector(".header__meuhb");
 const navList = document.querySelector(".header__nav")
-menuBtn.addEventListener("click", () => {
+
+
+function toogleMenu(){
   navList.classList.toggle("ativo")
   menuBtn.classList.toggle("menu-opacity")
-})
+}
+
+menuBtn.addEventListener("click",toogleMenu)
